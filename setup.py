8d0a781f8858e2664a6a37be97844819e3c98c7e
Wayne <liuzhy520@gmail.com>
2017-05-31 14:16:44 +0800

    setup.py added

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='PYWebChecker',
-    version='',
+    version='0.9',
     packages=['webspider.src.logger', 'webspider.src.spider', 'webspider.src.csvutil'],
     url='',
     license='',
     author='wayne',
-    author_email='',
+    author_email='liuzhy520@gmail.com',
     description=''
 )
