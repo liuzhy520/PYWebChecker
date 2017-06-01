1228e6493bdf7cbe0f5cbb3058541b1d0d12b68e
Wayne <liuzhy520@gmail.com>
2017-06-01 17:38:23 +0800

    change some names

--- webspider/src/run_spider.py
+++ webspider/src/run_spider.py
@@ -1,10 +1,12 @@
 from webspider.src.spider import wsdomestic
 from webspider.src.spider import skyscanner
+from webspider.src.logger import log
 
 url = "https://www.tianxun.com/oneway-ccan-csha.html?depdate=2017-05-27&cabin=Economy"
 
 def main():
     # wsdomestic.check_web(url)
+    log.v("program is starting and waiting for web respond")
     skyscanner.check_web("", "")
 
 main()
