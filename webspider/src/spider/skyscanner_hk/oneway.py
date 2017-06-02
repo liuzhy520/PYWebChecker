8f2fc8d7e6dd6c403163e4ca7329db224c8f5da1
Wayne <liuzhy520@gmail.com>
2017-06-02 18:16:20 +0800

    gangjiaoguo

---
+++ webspider/src/spider/skyscanner_hk/oneway.py
@@ -0,0 +1,11 @@
+from webspider.src.logger import log
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+mUrl = ""
+def setUrl(url):
+    mUrl = url
