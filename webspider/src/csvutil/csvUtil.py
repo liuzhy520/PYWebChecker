54978904dddc92203419292818c907f4a57e0158
Wayne <liuzhy520@gmail.com>
2017-06-07 18:32:11 +0800

    add the csv tool

---
+++ webspider/src/csvutil/csvUtil.py
@@ -0,0 +1,9 @@
+from webspider.src.spider.entity import return_entity
+
+returnEntity = return_entity
+
+def createReturnFile():
+    return ""
+
+def writeReturnEntity(returnEntity):
+    return returnEntity
